dcaed3347dfeaafe9cdb82b7663b8705a55aad4e
Bergur Hallgrímsson <bergur@bergur.is>
2021-01-11 19:08:31 +0000

    fix cookies

--- routes/logout.js
+++ routes/logout.js
@@ -1,12 +1,24 @@
 const Router = require('express').Router
 
-function makeLogoutRouter (logout) {
+function makeLogoutRouter (logout, isProduction) {
   const router = Router()
 
   router.post('/', (req, res, next) => {
     logout(req.user.token)
       .then(() => {
-        res.clearCookie('STUDNINGSBANKINN_API').sendStatus(200)
+        res
+          .clearCookie(
+            'STUDNINGSBANKINN_API',
+            {
+              domain: 'studningsbankinn.is',
+              secure: isProduction,
+              maxAge: 0,
+              httpOnly: true,
+              sameSite: true,
+              path: '/'
+            }
+          )
+          .sendStatus(200)
       })
       .catch(next)
   })
